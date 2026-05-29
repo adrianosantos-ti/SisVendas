44618baaf27277220b9bf026278c0fa3fc4614f2
Adriano Santos <168108332+adrianosantos-ti@users.noreply.github.com>
2026-05-29 17:14:20 -0300

    Update app_nuvem.py

--- app_nuvem.py
+++ app_nuvem.py
@@ -84,7 +84,7 @@ if not st.session_state['logado']:
             return base64.b64encode(img_file.read()).decode()
 
     # 2. Converte a sua logo (verifique se o nome do arquivo está certinho)
-    img_base64 = get_base64_image("Logo_dourada.jpeg")
+    img_base64 = get_base64_image("Apprimory_logo_branca")
 
     # 3. Exibe a imagem centralizada e com tamanho controlado (200px)
     st.markdown(
