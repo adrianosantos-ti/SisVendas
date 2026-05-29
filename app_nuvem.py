dc94549064bee2b2f08445f26907e4b97f179c93
Adriano Santos <168108332+adrianosantos-ti@users.noreply.github.com>
2026-05-29 15:43:54 -0300

    Update app_nuvem.py

--- app_nuvem.py
+++ app_nuvem.py
@@ -79,7 +79,7 @@ if not st.session_state['logado']:
     st.markdown(
         f"""
         <div style="text-align: center;">
-            <img src="data:image/png;base64,{img_base64}" width="200">
+            <img src="data:image/png;base64,{img_base64}" width="300">
         </div>
         """,
         unsafe_allow_html=True
